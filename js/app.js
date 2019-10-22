d442d1cf72ebd263451ce68be88782c5f4d1a642
Nick Socha <38092580+nicksocha@users.noreply.github.com>
2019-10-21 21:27:28 -0500

    v1.0 Done
    v1.0 Done - Cleaned code

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// Header Characteristics Carousel
 const TxtRotate = function(el, toRotate, period) {
   this.toRotate = toRotate;
   this.el = el;
@@ -81,6 +82,27 @@ const myArray = [
 shuffle(myArray);
 
 // Automatically update year in footer
-
 const yyyy = new Date().getFullYear();
 document.getElementById('year').innerHTML = `Nick Socha dot com #${yyyy}`;
+
+// startTypeform
+(function() {
+  let qs;
+  let js;
+  let q;
+  let s;
+  const d = document;
+  const gi = d.getElementById;
+  const ce = d.createElement;
+  const gt = d.getElementsByTagName;
+  const id = 'typef_orm_share';
+  const b = 'https://embed.typeform.com/';
+  if (!gi.call(d, id)) {
+    js = ce.call(d, 'script');
+    js.id = id;
+    js.src = `${b}embed.js`;
+    q = gt.call(d, 'script')[0];
+    q.parentNode.insertBefore(js, q);
+  }
+})();
+// endTypeform
